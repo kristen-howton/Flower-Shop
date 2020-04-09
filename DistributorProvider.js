31bb6458046f76f6733b6d23595454b3f4b68cda
kristen-howton <kristen.howton@gmail.com>
2020-04-09 11:02:56 -0500

    removing distrubtors that I forgot outside of scripts

--- DistributorProvider.js
+++
@@ -1,7 +0,0 @@
-let distributors = []
-
-export const useDistributors = () => distributors.slice()
-
-export const getDistributors = () => fetch("http://localhost:3000/distributors")
-    .then(response => response.json())
-    .then(parsedDistributors => distributors = parsedDistributors)
